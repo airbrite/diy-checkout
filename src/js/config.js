e0bbc30ba4cf3204c167c1fb30595948e723ee40
Kelly Miyashiro <kmiyashiro@users.noreply.github.com>
2014-08-27 17:58:31 -0700

    Comment test settings

--- src/js/config.js
+++ src/js/config.js
@@ -2,7 +2,7 @@
 
 define(function(require) {
   return {
-    apiHost: 'https://api-sandbox.trycelery.com',
-    slug: '53ebdd5e1fd9c90400553dab'
+    // apiHost: 'https://api-sandbox.trycelery.com',
+    // slug: '53ebdd5e1fd9c90400553dab'
   };
 })
