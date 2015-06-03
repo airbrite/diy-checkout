c64a4ababad64d893ea1ccf4a3b9e4f6a650d4ba
benjipott <pott.benjamin@gmail.com>
2015-06-03 11:20:18 +0200

    Add templating i18n

--- src/js/confirmation.js
+++ src/js/confirmation.js
@@ -1,109 +1,109 @@
-define(function(require) {
-  var template = require('templates/index').confirmation;
-  var shopData = require('shop').data;
+define(function (require) {
+    var template = require('templates/index').confirmation;
+    var shopData = require('shop').data;
+    var locale = require('../locale/fr');
+    // Icons
+    var checkIcon = require('text!templates/svg/check.svg');
+    var emailIcon = require('text!templates/svg/email.svg');
+    var facebookIcon = require('text!templates/svg/facebook.svg');
+    var twitterIcon = require('text!templates/svg/twitter.svg');
 
-  // Icons
-  var checkIcon = require('text!templates/svg/check.svg');
-  var emailIcon = require('text!templates/svg/email.svg');
-  var facebookIcon = require('text!templates/svg/facebook.svg');
-  var twitterIcon = require('text!templates/svg/twitter.svg');
+    return {
+        initialize: function (data) {
+            data = data || {number: 1};
+            this.data = data || this.data || {};
+            this.render();
+            return this;
+        },
 
-  return {
-    initialize: function(data) {
-      data = data || {number: 1};
-      this.data = data || this.data || {};
-      this.render();
-      return this;
-    },
+        $el: null,
+        template: template,
+        render: function (data) {
+            data = $.extend(data || this.data || {}, locale.confirmation);
+            data.twitterHref = this.generateTwitterHref();
+            data.facebookHref = this.generateFacebookHref();
+            data.mailHref = this.generateMailHref();
 
-    $el: null,
-    template: template,
-    render: function(data) {
-      data = data || this.data || {};
-      data.twitterHref = this.generateTwitterHref();
-      data.facebookHref = this.generateFacebookHref();
-      data.mailHref = this.generateMailHref();
+            var $el = this.$el = $(this.template(data));
 
-      var $el = this.$el = $(this.template(data));
+            this._appendIcons();
 
-      this._appendIcons();
+            return this;
+        },
 
-      return this;
-    },
+        generateFacebookHref: function (data) {
+            var website = this._getWebsite();
+            var slug = this._getSlug();
 
-    generateFacebookHref: function(data) {
-      var website = this._getWebsite();
-      var slug = this._getSlug();
+            var qs = $.param({
+                u: website
+            });
 
-      var qs = $.param({
-        u: website
-      });
+            return 'https://www.facebook.com/sharer/sharer.php?' + qs;
+        },
 
-      return 'https://www.facebook.com/sharer/sharer.php?' + qs;
-    },
+        generateTwitterHref: function (data) {
+            var twitterHandle = shopData.twitter;
+            var productName = this._getProductName();
+            var website = this._getWebsite();
+            var slug = this._getSlug();
 
-    generateTwitterHref: function(data) {
-      var twitterHandle = shopData.twitter;
-      var productName = this._getProductName();
-      var website = this._getWebsite();
-      var slug = this._getSlug();
+            // Build the share text string
+            var pieces = [];
 
-      // Build the share text string
-      var pieces = [];
+            if (productName) {
+                pieces.push('Check out ' + productName);
+            }
 
-      if (productName) {
-        pieces.push('Check out ' + productName);
-      }
+            if (twitterHandle) {
+                pieces.push('from ' + twitterHandle);
+            }
 
-      if (twitterHandle) {
-        pieces.push('from ' + twitterHandle);
-      }
+            pieces.push('on @trycelery');
 
-      pieces.push('on @trycelery');
+            var qs = $.param({
+                url: website,
+                text: pieces.join(' ')
+            });
 
-      var qs = $.param({
-        url: website,
-        text: pieces.join(' ')
-      });
+            return 'http://twitter.com/share?' + qs;
+        },
 
-      return 'http://twitter.com/share?' + qs;
-    },
+        generateMailHref: function (data) {
+            var productName = this._getProductName();
+            var website = this._getWebsite();
 
-    generateMailHref: function(data) {
-      var productName = this._getProductName();
-      var website = this._getWebsite();
+            return 'mailto:?subject=Check out ' + productName + '&body=Check out ' + productName + ': ' + website;
+        },
 
-      return 'mailto:?subject=Check out ' + productName + '&body=Check out ' + productName + ': ' + website;
-    },
+        _appendIcons: function () {
+            var $el = this.$el;
+            var $check = $(checkIcon);
+            var $twitter = $(twitterIcon);
+            var $facebook = $(facebookIcon);
+            var $email = $(emailIcon);
 
-    _appendIcons: function() {
-      var $el = this.$el;
-      var $check = $(checkIcon);
-      var $twitter = $(twitterIcon);
-      var $facebook = $(facebookIcon);
-      var $email = $(emailIcon);
+            $el.find('.Celery-Icon--check').append($check);
+            $el.find('.Celery-Icon--twitter').append($twitter);
+            $el.find('.Celery-Icon--facebook').append($facebook);
+            $el.find('.Celery-Icon--email').append($email);
+        },
 
-      $el.find('.Celery-Icon--check').append($check);
-      $el.find('.Celery-Icon--twitter').append($twitter);
-      $el.find('.Celery-Icon--facebook').append($facebook);
-      $el.find('.Celery-Icon--email').append($email);
-    },
+        _getSlug: function () {
+            return this._getProduct()._id;
+        },
 
-    _getSlug: function() {
-      return this._getProduct()._id;
-    },
+        _getProduct: function (data) {
+            return shopData.product || {};
+        },
 
-    _getProduct: function(data) {
-      return shopData.product || {};
-    },
+        _getProductName: function (data) {
+            return this._getProduct().name;
+        },
 
-    _getProductName: function(data) {
-      return this._getProduct().name;
-    },
-
-    _getWebsite: function() {
-      var slug = this._getSlug();
-      return shopData.website || 'https://shop.trycelery.com/page/' + slug;
+        _getWebsite: function () {
+            var slug = this._getSlug();
+            return shopData.website || 'https://shop.trycelery.com/page/' + slug;
+        }
     }
-  }
 });
---
+++ src/locale/fr.js
@@ -0,0 +1,40 @@
+define(function () {
+    {
+        return {
+            modal: {
+                preorder: 'Pre-Order for 50% Off!',
+                charged: 'Your card will be charged immediately',
+                footer: 'We plan to start shipping summer 2014.<br />We will send you an email to collect your address later.'
+            },
+            overlay: {},
+            confirmation: {
+                thanks: 'Thank you<br /> for your support!',
+                emailConfirm: 'Your pre-order number is #{{number}}. A confirmation email is on the way to your inbox.',
+                share: 'Share with your friends!'
+            },
+            form: {
+                quantity: 'Quantity',
+                shipping: 'Shipping',
+                contry: {
+                    us: {
+                        name: 'United States',
+                        value: 'us'
+                    },
+                    int: {
+                        name: 'International',
+                        value: 'zz'
+                    }
+                },
+                contact: 'Contact Information',
+                pay: 'Payment',
+                promo: 'Promo Code',
+                promoType: 'Type your code',
+                qty: 'Qty',
+                ship: 'Ship',
+                taxes: 'Taxes',
+                preorder: 'Pre-Order Now',
+                secure: ' This page is secured with 128-bit encryption',
+            }
+        }
+    }
+});
