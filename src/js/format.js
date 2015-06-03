93fc53b858af86474715692ff70d977d3082b85c
benjipott <pott.benjamin@gmail.com>
2015-06-03 13:39:19 +0200

    update locale money

--- src/js/format.js
+++ src/js/format.js
@@ -1,17 +1,21 @@
-define(function(require) {
-  var accounting = require('vendor/accounting.js/accounting');
+define(function (require) {
+    var accounting = require('vendor/accounting.js/accounting');
+    var locale = require('../locale/fr').money;
+    return {
+        formatMoney: function (cents) {
+            var options = {
+                precision: 0,
+                symbol: locale.symbol,
+                format: locale.format
+            };
+            var dollars = cents / 100;
 
-  return {
-    formatMoney: function(cents) {
-      var options = { precision: 2 };
-      var dollars = cents / 100;
+            // Don't show cents when .00
+            if (dollars % 1 === 0) {
+                options.precision = 0;
+            }
 
-      // Don't show cents when .00
-      if (dollars % 1 === 0) {
-        options.precision = 0;
-      }
-
-      return accounting.formatMoney(dollars, options);
+            return accounting.formatMoney(dollars, options);
+        }
     }
-  }
 });
--- src/locale/fr.js
+++ src/locale/fr.js
@@ -2,22 +2,27 @@ define(function () {
     {
         return {
             modal: {
-                preorder: 'Pre-Order for 50% Off!',
-                charged: 'Your card will be charged immediately',
-                footer: 'We plan to start shipping summer 2014.<br />We will send you an email to collect your address later.'
+                preorder: 'Pré-commander pour 50% de réduction!',
+                charged: 'Votre carte sera débitée immédiatement',
+                footer: 'Nous prévoyons de commencer l\'été expédition 2014. \n Nous vous enverrons un e-mail pour recueillir votre adresse plus tard'
             },
             overlay: {},
             confirmation: {
-                thanks: 'Thank you<br /> for your support!',
-                emailConfirm: 'Your pre-order number is #{{number}}. A confirmation email is on the way to your inbox.',
-                share: 'Share with your friends!'
+                thanks: 'Merci <br /> pour votre soutien !',
+                emailConfirm: 'Votre numéro de pré-commande est #{{number}}. Un email de confirmation à été envoyé votre boîte de réception .',
+                share: 'Partagez avec vos amis !'
             },
             form: {
-                quantity: 'Quantity',
-                shipping: 'Shipping',
+                quantity: 'Quantité',
+                shipping: 'Livraison',
+                card: {
+                    number: 'Numéro de carte de crédit',
+                    date: 'MM / AA',
+                    cvc: 'cv'
+                },
                 contry: {
                     us: {
-                        name: 'United States',
+                        name: 'États Unis',
                         value: 'us'
                     },
                     int: {
@@ -25,15 +30,19 @@ define(function () {
                         value: 'zz'
                     }
                 },
-                contact: 'Contact Information',
-                pay: 'Payment',
-                promo: 'Promo Code',
-                promoType: 'Type your code',
-                qty: 'Qty',
-                ship: 'Ship',
-                taxes: 'Taxes',
-                preorder: 'Pre-Order Now',
-                secure: ' This page is secured with 128-bit encryption',
+                money: {
+                    symbol: '€',
+                    format: '%v%s'
+                },
+                contact: 'Coordonnées',
+                pay: 'Paiement',
+                promo: 'Code Promo',
+                promoType: 'Tapez votre code',
+                qty: 'Quantité',
+                ship: 'Transport',
+                taxes: 'Tva',
+                preorder: 'Pré-commandez maintenant',
+                secure: 'Cette page est sécurisée avec un cryptage 128-bit',
             }
         }
     }
